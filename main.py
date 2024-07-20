b2e8f43e0494290f772bb3f7539719bba7670f95
jonDScode <jonnathanospinam@gmail.com>
2024-07-21 00:04:27 +0200

    subiendo archivos

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos= ['TODO 1', 'TODO 2', 'TODO 3']
+
+@app.route('/')
+def index():
+
+
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response 
+
+@app.route('/hello')
+def hello():
+
+    user_ip = request.cookies.get('user_ip')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+
+    return render_template('hello.html', **context)
+
+    if __name__ == "__main__":
+     app.run(port=5000, debug=True)  
